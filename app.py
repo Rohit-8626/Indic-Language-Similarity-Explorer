cb72c1a4e34e8d7edca6814beaeb1c70614ac518
Rohit Vastani <rohitvastani8626@gmail.com>
2025-12-03 19:08:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import os
 import streamlit as st
 from sklearn.metrics.pairwise import cosine_similarity
 
+HF_TOKEN = st.secrets.get("huggingface", {}).get("token")
+
 language_centroids = joblib.load('language_centroids.pkl')
 KMeans = joblib.load("Kmeans_Cluster_Indic_Language_model.pkl")
 
@@ -14,12 +16,12 @@ model_name = 'ai4bharat/indic-bert'
 
 @st.cache_resource
 def load_tokenizer(model_name):
-    return AutoTokenizer.from_pretrained(model_name)
+    return AutoTokenizer.from_pretrained(model_name , token = HF_TOKEN)
 tokenizer = load_tokenizer(model_name)
 
 @st.cache_resource
 def load_model(model_name):
-    return AutoModel.from_pretrained(model_name)
+    return AutoModel.from_pretrained(model_name , token = HF_TOKEN)
 model = load_model(model_name)
 model.to(device)
 model.eval()
@@ -66,5 +68,6 @@ if st.button("Analyze"):
         else:
 
             st.write("Dravidian Language")
+
 
 
